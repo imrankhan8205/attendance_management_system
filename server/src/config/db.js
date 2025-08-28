03423dbe1681045462c2c8dfb89736c3e44e14c4
Muskan-23456789 <muskanmashrak654@gmail.com>
2025-08-28 21:13:38 +0530

    do

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -19,7 +19,8 @@ const connectDB = async () => {
 
     const conn = await mongoose.connect(uri);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
-  } catch (error) {
+  }
+   catch (error) {
     console.error(`Error: ${error.message}`);
     process.exit(1);
   }
